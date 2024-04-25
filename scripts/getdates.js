7baf2f85b311694ff8447cc94795be3d349a5b79
LegendaryEmp <jacobokwusi@gmail.com>
2024-04-25 19:10:06 +0100

    Assignments

---
+++ scripts/getdates.js
@@ -0,0 +1,13 @@
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+let currentYearElement = document.getElementById("currentyear");
+currentYearElement.textContent = currentYear;
+
+function displayLastModified() {
+    var lastModified = new Date(document.lastModified);
+    var formattedDate = lastModified.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+    document.getElementById('lastmodified').textContent = 'Last modified: ' + formattedDate;
+}
+
+window.onload = displayLastModified;
