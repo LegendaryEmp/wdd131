9b4623f2364c9d79799fc7c04cc456960d4cbbb8
Jacob Okwusi Mbabo <jacobokwusi@gmail.com>
2025-01-27 19:25:11 +0100

    week03 assignment

---
+++ scripts/place.js
@@ -0,0 +1,32 @@
+function calculateWindChill(temperature, windSpeed) {
+    
+    if (temperature <= 10 && windSpeed > 4.8) {
+        return 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+    } else {
+        return temperature;
+    }
+}
+
+const temperature = 32; 
+const windSpeed = 13; 
+
+
+const windChill = calculateWindChill(temperature, windSpeed).toFixed(2);
+
+
+document.getElementById('wind-chill').innerText = `${windChill} °C`;
+
+
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+let currentYearElement = document.getElementById("currentyear");
+currentYearElement.textContent = currentYear;
+
+function displayLastModified() {
+    var lastModified = new Date(document.lastModified);
+    var formattedDate = lastModified.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+    document.getElementById('lastmodified').textContent = 'Last modified: ' + formattedDate;
+}
+
+window.onload = displayLastModified;
